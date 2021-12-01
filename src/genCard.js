ed27a613275fd382b810e892ed786803fc6f8f0d
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2021-11-30 18:41:55 -0800

    final commit

--- src/genCard.js
+++ src/genCard.js
@@ -1,8 +1,8 @@
-function genManagerCard (manager) {
-  const managerCard = `<div class="card">
+function genManagerCard(manager) {
+    const managerCard = `<div class="card">
   <div class="card-title">
       <h3 class="name">${manager.name}</h3>
-      <h4 class="type">Manager</h4>
+      <h4 class="type"> <span class="fas fa-mug-hot"></span> Manager</h4>
   </div>
   <div class="card-body">
       <p>Employee ID: <span class="ID">${manager.id}</span></p>
@@ -10,17 +10,17 @@ function genManagerCard (manager) {
       <p>Office Number: <span class="officeNumber">${manager.officeNumber}</span></p>
   </div>
 </div>`
-return managerCard
+    return managerCard
 }
 
-function genEngineerCards (engineers) {
-  let engineerCards = ``
-  for(let i = 0; i < engineers.length; i++) {
-      let engineer = engineers[i]
-      let card = `<div class="card">
+function genEngineerCards(engineers) {
+    let engineerCards = ``
+    for (let i = 0; i < engineers.length; i++) {
+        let engineer = engineers[i]
+        let card = `<div class="card">
       <div class="card-title">
           <h3 class="name">${engineer.name}</h3>
-          <h4 class="type">Engineer</h4>
+          <h4 class="type"><span class="fas fa-rss"></span> Engineer</h4>
       </div>
       <div class="card-body">
           <p>Employee ID: <span class="ID">${engineer.id}</span></p>
@@ -28,20 +28,20 @@ function genEngineerCards (engineers) {
           <p>Github: <span class="Github"><a href="https://www.github.com/${engineer.github}"  target="_blank">${engineer.github}</a></span></p>
       </div>
   </div>`
-  engineerCards += card
-  }
-  return engineerCards
+        engineerCards += card
+    }
+    return engineerCards
 }
 
 
-function genInternCards (interns) {
-  let internCards = ``
-  for(let i = 0; i < interns.length; i++) {
-      let intern = interns[i]
-      let card = `<div class="card">
+function genInternCards(interns) {
+    let internCards = ``
+    for (let i = 0; i < interns.length; i++) {
+        let intern = interns[i]
+        let card = `<div class="card">
       <div class="card-title">
           <h3 class="name">${intern.name}</h3>
-          <h4 class="type">Intern</h4>
+          <h4 class="type"><span class="fas fa-user-graduate"></span> Intern</h4>
       </div>
       <div class="card-body">
           <p>Employee ID: <span class="ID">${intern.id}</span></p>
@@ -50,9 +50,9 @@ function genInternCards (interns) {
       </div>
   </div>`
 
-  internCards += card
-  }
-  return internCards
+        internCards += card
+    }
+    return internCards
 }
 
 
--- src/genHTML.js
+++ src/genHTML.js
@@ -1,42 +1,29 @@
 function genHTML(managerCard, engineerCards, internCards) {
-  const innerHTML = `<!DOCTYPE html>
+    const innerHTML = `<!DOCTYPE html>
   <html lang="en">
   <head>
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>My Team</title>
-      <link rel="stylesheet" href="./dist/css/style.css">
-  </head>
-  <body>
+      <link rel="stylesheet" href="./css/style.css">
+      <script src="https://kit.fontawesome.com/f14c85c842.js" crossorigin="anonymous"></script>
+
+    </head>
+    <body>
       <header>
           <h1>My Team</h1>
       </header>
-      <main>
-          <div class="manager">
-              <div class="manager-card">
-                  <h2>Manager</h2>
-                  ${managerCard}
-              </div>
-          </div>
-          <div class="employees">
-              <div class="engineers">
-                  <h2>Engineers</h2>
-                  <div class="engineer-cards">
-                      ${engineerCards}
-                  </div>
-              </div>
-              <div class="interns">
-                  <h2>Interns</h2>
-                  <div class="intern-cards">
-                      ${internCards}
-                  </div>
-              </div>
-          </div>
-      </main>
+        <main>
+            <div class="employees">
+                ${managerCard}
+                ${engineerCards}
+                ${internCards}
+            </div>
+        </main>
   </body>
   </html>`
-  return innerHTML
+    return innerHTML
 }
 
 module.exports = genHTML
